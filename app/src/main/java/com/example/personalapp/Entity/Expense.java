3208e699352d8b272c98ab007aa5e8d4bdae758a
EnKr99 <JJINGHHUI99.18@ICHAT.SP.EDU.SG>
2020-01-31 18:45:00 +0800

    init
    --- jinghui

---
+++ app/src/main/java/com/example/personalapp/Entity/Expense.java
@@ -0,0 +1,65 @@
+package com.example.personalapp.Entity;
+
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "expense_table")
+public class Expense {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String memo;
+
+    private Double money;
+
+    private String date;
+
+    public Expense(String memo, double money) {
+        this.memo = memo;
+        this.money = money;
+    }
+
+//    public Expense(int id, String memo, double money) {
+//        this.id = id;
+//        this.memo = memo;
+//        this.money = money;
+//    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void getToday(String today) {
+
+    }
+}
