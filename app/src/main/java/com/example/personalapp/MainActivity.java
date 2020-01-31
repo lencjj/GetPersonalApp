69264bbf2b82c5f9bbbc798c0c53ea8d6c9b450b
lencjj <lennelchua@gmail.com>
2020-01-31 18:50:06 +0800

    Version 1 of profile picture activity

--- app/src/main/java/com/example/personalapp/MainActivity.java
+++ app/src/main/java/com/example/personalapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.personalapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
@@ -9,26 +10,38 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.auth.User;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import org.w3c.dom.Text;
 
+import java.io.ByteArrayOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -49,6 +62,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     SimpleDateFormat eventDateFormat = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
     DisplayMetrics metrics = new DisplayMetrics();
     public static int dpi;
+    private ImageView profPicView;
+    private int REQUEST_IMAGE_CAPTURE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -84,23 +99,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //get dpi of the screen
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
-
-        //get height of the non grid view items
-
         dpi = metrics.densityDpi;
 
-
-
-
+        //Handle logout button
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
-
         navigationView = (NavigationView) findViewById(R.id.navigationView);
         Menu menu = (Menu) navigationView.getMenu();
         logoutItem = (MenuItem) menu.getItem(1);
-        View view = (View) navigationView.getHeaderView(0);
-        displayUName = (TextView) view.findViewById(R.id.username);
-        getUsername();
-
         logoutItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
@@ -109,6 +114,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Intent j = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(j);
                 return true;
+            }
+        });
+
+        //Retrieve and display username
+        View view = (View) navigationView.getHeaderView(0);
+        displayUName = (TextView) view.findViewById(R.id.username);
+        getUsername();
+
+
+        //Handle profile picture activities
+        profPicView = (ImageView) view.findViewById(R.id.displayPicture);
+        if(user.getPhotoUrl() != null){
+            Glide.with(this)
+                    .load(user.getPhotoUrl())
+                    .into(profPicView);
+        }
+        ImageButton pictureBtn = (ImageButton) view.findViewById(R.id.pictureBtn);
+        pictureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePictureIntent();
             }
         });
 
@@ -136,7 +162,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    public void getUsername(){
+    private void getUsername(){
         docRef.get()
                 .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                     @Override
@@ -158,7 +184,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 });
     }
 
-    public int getNotification(String date){
+    private int getNotification(String date){
         int i = 0;
         sqDbHelper = new DBHelper(MainActivity.this);
         SQLiteDatabase database = sqDbHelper.getReadableDatabase();
@@ -170,5 +196,80 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         sqDbHelper.close();
 
         return i;
+    }
+
+    private void takePictureIntent(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_IMAGE_CAPTURE){
+            switch(resultCode){
+                case RESULT_OK:
+                    Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                    profPicView.setImageBitmap(bitmap);
+                    uploadImage(bitmap);
+
+            }
+        }
+    }
+
+    private void uploadImage(Bitmap bitmap){
+        ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outStream);
+
+        String id = firebaseAuth.getInstance().getCurrentUser().getUid();
+        final StorageReference reference = FirebaseStorage.getInstance().getReference()
+                .child("profileImgs")
+                .child(id + ".jpeg");
+
+        reference.putBytes(outStream.toByteArray())
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        getDownloadUrl(reference);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e("ProfilePictureActivity", "Failed: ", e.getCause());
+            }
+        });
+    }
+
+    private void getDownloadUrl(StorageReference reference){
+        reference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Log.d("ProfilePictureActivity", "Sucess: " + uri);
+                setUserProfileUrl(uri);
+            }
+        });
+    }
+
+    private void setUserProfileUrl(Uri uri){
+        FirebaseUser user = firebaseAuth.getInstance().getCurrentUser();
+
+        UserProfileChangeRequest request = new UserProfileChangeRequest.Builder()
+                .setPhotoUri(uri)
+                .build();
+
+        user.updateProfile(request)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(MainActivity.this, "Profile image update success..", Toast.LENGTH_SHORT);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, "Profile image update failed...", Toast.LENGTH_SHORT);
+            }
+        });
     }
 }
